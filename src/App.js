1092aeee55ad015cb7bb5e8c3f7e74e42b7499b1
tanaydave <114003362+tanaydave@users.noreply.github.com>
2025-03-18 15:01:17 +0530

    minor changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 // src/App.js
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-    </div>
+    <Router>
+         <Routes>
+           <Route exact path="/" component={<Home/>} />
+           {/* Add more routes here if needed */}
+         </Routes>
+       </Router>
   );
 }
 
